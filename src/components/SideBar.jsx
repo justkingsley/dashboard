9dd2d7c4652f612f7b5f294c825a0a3893687489
justkingsley <kingsleybingala@gmail.com>
2023-09-07 15:51:22 +0200

    Sidebar Logo
    Sidebar panel now has a logo and is beginning to take shape. 😁

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -4,9 +4,24 @@ import { SiShopware } from 'react-icons/si';
 import { MdOutlineCancel } from 'react-icons/md';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
+import { links } from '../data/dummy';
+
 const Sidebar = () => {
+
+  const activeMenu = true;
+
   return (
-    <div>Sidebar</div>
+    <div className='ml-3 h-screen md:overflow-hidden md:small:overflow-auto overflow-auto pb-10'>
+      {activeMenu && (
+        <>
+          <div className='flex justify-between items-center'>
+            <Link to='/' onClick={() => {}} className='items-center'>
+              <SiShopware /> <span>Shopify</span>
+            </Link>
+          </div>
+        </>
+      )}
+    </div>
   )
 }
 
