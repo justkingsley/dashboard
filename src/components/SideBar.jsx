68a7063d7235372bd6f82edb358373ab6b90cbed
justkingsley <kingsleybingala@gmail.com>
2023-09-08 09:36:03 +0200

    Sidebar
    Sidebar close section design.

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -15,8 +15,17 @@ const Sidebar = () => {
       {activeMenu && (
         <>
           <div className='flex justify-between items-center'>
-            <Link to='/' onClick={() => {}} className='items-center'>
+            <Link to='/' onClick={() => {}} className='items-center gap-3 flex ml-3 mt-4 text-xl font-extrabold tracking-tight dark:text-white text-slate-800'>
               <SiShopware /> <span>Shopify</span>
+              <TooltipComponent
+                content='Menu' position='BottomCenter'>
+            
+              <button type='button' onClick={() => {}}
+                className='text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden'
+              >
+                <MdOutlineCancel />
+              </button>
+              </TooltipComponent>
             </Link>
           </div>
         </>
