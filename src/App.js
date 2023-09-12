b86f9ab0064dab50225b1ae312f583175062978a
justkingsley <kingsleybingala@gmail.com>
2023-09-12 15:41:18 +0200

    Update App.js
    activeMenu now using useStateContext.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { userEffect } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { FiSettings } from 'react-icons/fi';
 import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { useStateContext } from './contexts/ContextProvider';
 
 
 import { Navbar, Footer, Sidebar, ThemeSettings } from './components';
@@ -10,7 +11,7 @@ import './App.css';
 
 const App = () => {
 
-    const activeMenu = true;
+    const { activeMenu } = useStateContext();
 
     return (
         <div>
